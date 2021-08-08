3528d88cdb199f946da5215ece17e44cc589696e
Sajid Mahmood <sajidm46@hotmail.com>
2021-08-08 20:10:40 +0100

    console application created

---
+++ RockPaperScissorConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RockPaperScissorConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
