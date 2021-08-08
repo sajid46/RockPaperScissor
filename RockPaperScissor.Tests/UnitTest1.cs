37f15af6ccb2fb216b6fc882ea466590a87c6911
Sajid Mahmood <sajidm46@hotmail.com>
2021-08-08 22:54:32 +0100

    Test project added

---
+++ RockPaperScissor.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RockPaperScissor.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
